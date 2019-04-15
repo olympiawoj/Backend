5ec05b18f2cf5ef276fff641176fb7433ed26c83
olympia <owojcik7@gmail.com>
2019-04-15 14:24:35 -0400

    Create register, login, and get users endpoints

---
+++ routers/users-router.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+import Users from "users-helpers.js";
+
+//Register or CREATE- takes in username, email, password, returns object with new user
+router.post("/register", async (req, res) => {
+  let user = req.body;
+  if (!user.username || !user.email || !user.password) {
+    res.status(401).json({
+      message: "Username, email, and password are required for registration"
+    });
+  }
+
+  try {
+    const newUser = await Users.register(user);
+    res.status(200).json(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error registering user" });
+  }
+});
+
+//LOGIN- auth required, takes in username and password, returns message, username, and token
+//To do: Create token, add bcrypt, and return token along with message.
+router.post("/login", async (req, res) => {
+  let { username, password } = req.body;
+  try {
+    if (username && password) {
+      res,
+        status(200).json({
+          message: `Welcome ${user.username}`,
+          username,
+          token
+        });
+    } else {
+      res.status(401).json({ message: "Please provide username and password" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET list of all users - auth required
+router.get("/users", async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error getting list of users" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
+const userRouter = require("../routers/users-router.js");
+
+server.use("/api", userRouter);
+
 server.get("/", (req, res) => {
   res.send("testing the server");
 });
