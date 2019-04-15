2adbfba0014cf0475355e08c63a26e3c5aae9a4e
olympia <owojcik7@gmail.com>
2019-04-15 18:04:34 -0400

    Add cors to server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const cors = require("cors");
 const server = express();
 
 const usersRouter = require("./routers/users-router.js");
 
+server.use(cors());
 server.use("/api", usersRouter);
 
 server.get("/", (req, res) => {
